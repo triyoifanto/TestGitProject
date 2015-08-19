7ac54d828ff4728e565b70b3fc28c946c2279da7
triyoifanto <triyo.ifanto@mitrais.com>
2015-08-19 09:26:58 +0800

    test commit and push

---
+++ GittestWebApp/GittestWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GittestWebApp.MvcApplication" Language="C#" %>
---
+++ GittestWebApp/GittestWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GittestWebApp.Startup))]
+namespace GittestWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
